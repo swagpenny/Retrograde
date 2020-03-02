2e29ed8d3f013323780cb3bfd454212b458ee7b3
jcp3213 <42848748+jcp3213@users.noreply.github.com>
2020-03-02 15:01:13 -0500

    Added the textbox and buttons and made changes to the Manager

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -1,18 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Manager : MonoBehaviour
 {
+
+    //public Text playerHealth;
+    //public Text enemyHealth;
+    public Text enemyAction;
+    public Text playerAction;
+    public Combat combat;
+    //public Player player;
+    //public Enemy enemy;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        //playerHealth.text = "Player Health: " + player.health;
+        //enemyHealth.text = "Enemy Health: " + enemy.health;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        //enemyAction.text = "The enemy intends to deal " + combat.placeholderNameForVariable1 + " damage to you and block " + combat.placeholderNameForVariable2 + " incoming damage.";
+        //playerAction.text = "The enemy intends to deal " + combat.placeholderNameForVariable3 + " damage to you and block " + combat.placeholderNameForVariable4 + " incoming damage.";
+
     }
 }
